f48203b1099fe81358465ce67709d43b2eacd446
aushacker <25114336+aushacker@users.noreply.github.com>
2022-10-06 11:01:12 +1100

    Rework tracing init

--- nodejs/tracing2.js
+++ nodejs/tracing2.js
@@ -1,25 +1,54 @@
-const opentelemetry = require("@opentelemetry/sdk-node");
+const { HttpInstrumentation } = require("@opentelemetry/instrumentation-http");
+const { ExpressInstrumentation } = require("@opentelemetry/instrumentation-express");
+const opentelemetry = require("@opentelemetry/api");
+const { Resource } = require("@opentelemetry/resources");
+const { SemanticResourceAttributes } = require("@opentelemetry/semantic-conventions");
+const { NodeTracerProvider } = require("@opentelemetry/sdk-trace-node");
+const { registerInstrumentations } = require("@opentelemetry/instrumentation");
+const { ConsoleSpanExporter, BatchSpanProcessor } = require("@opentelemetry/sdk-trace-base");
+
+const otelsdk = require("@opentelemetry/sdk-node");
 const {
   getNodeAutoInstrumentations,
 } = require("@opentelemetry/auto-instrumentations-node");
 const {
   OTLPTraceExporter,
 } = require("@opentelemetry/exporter-trace-otlp-http");
-const { HttpInstrumentation } = require('@opentelemetry/instrumentation-http');
-const { ExpressInstrumentation } = require('@opentelemetry/instrumentation-express');
 
-const exporterUrl = process.env.OTLP_URL || "http://localhost:4318/v1/traces";
-
-const sdk = new opentelemetry.NodeSDK({
+const sdk = new otelsdk.NodeSDK({
   traceExporter: new OTLPTraceExporter({
     // optional - url default value is http://localhost:4318/v1/traces
-    url: exporterUrl,
+    url: process.env.OTLP_URL || "localhost:4318/v1/traces",
     // optional - collection of custom headers to be sent with each request, empty by default
     headers: {},
   }),
-  instrumentations: [getNodeAutoInstrumentations(),
+  instrumentations: [getNodeAutoInstrumentations()],
+});
+sdk.start();
+
+// This registers all instrumentation packages
+registerInstrumentations({
+  instrumentations: [
+    // Express instrumentation expects HTTP layer to be instrumented
+    getNodeAutoInstrumentations(),
     new HttpInstrumentation(),
     new ExpressInstrumentation(),
   ],
 });
-sdk.start();
+
+const resource =
+  Resource.default().merge(
+    new Resource({
+      [SemanticResourceAttributes.SERVICE_NAME]: "otel-nodejs",
+      [SemanticResourceAttributes.SERVICE_VERSION]: "0.1.0",
+    })
+  );
+
+const provider = new NodeTracerProvider({
+    resource: resource,
+});
+const exporter = new ConsoleSpanExporter();
+const processor = new BatchSpanProcessor(exporter);
+provider.addSpanProcessor(processor);
+
+provider.register();
