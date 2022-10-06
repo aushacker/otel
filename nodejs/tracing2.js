7c085504163307fafd07369b7f41b4b9b1716738
aushacker <25114336+aushacker@users.noreply.github.com>
2022-10-06 13:09:28 +1100

    cleanup part 1

--- nodejs/tracing2.js
+++ nodejs/tracing2.js
@@ -1,30 +1,16 @@
+const { registerInstrumentations } = require("@opentelemetry/instrumentation");
+const { getNodeAutoInstrumentations } = require("@opentelemetry/auto-instrumentations-node");
 const { HttpInstrumentation } = require("@opentelemetry/instrumentation-http");
 const { ExpressInstrumentation } = require("@opentelemetry/instrumentation-express");
-const opentelemetry = require("@opentelemetry/api");
+
+//const opentelemetry = require("@opentelemetry/api");
 const { Resource } = require("@opentelemetry/resources");
 const { SemanticResourceAttributes } = require("@opentelemetry/semantic-conventions");
+
 const { NodeTracerProvider } = require("@opentelemetry/sdk-trace-node");
-const { registerInstrumentations } = require("@opentelemetry/instrumentation");
-const { ConsoleSpanExporter, BatchSpanProcessor } = require("@opentelemetry/sdk-trace-base");
-
-const otelsdk = require("@opentelemetry/sdk-node");
-const {
-  getNodeAutoInstrumentations,
-} = require("@opentelemetry/auto-instrumentations-node");
-const {
-  OTLPTraceExporter,
-} = require("@opentelemetry/exporter-trace-otlp-http");
-
-//const sdk = new otelsdk.NodeSDK({
-//  traceExporter: new OTLPTraceExporter({
-    // optional - url default value is http://localhost:4318/v1/traces
-//    url: process.env.OTLP_URL || "localhost:4318/v1/traces",
-    // optional - collection of custom headers to be sent with each request, empty by default
-//    headers: {},
-//  }),
-//  instrumentations: [getNodeAutoInstrumentations()],
-//});
-//sdk.start();
+//const { ConsoleSpanExporter, BatchSpanProcessor } = require("@opentelemetry/sdk-trace-base");
+const { OTLPTraceExporter } = require("@opentelemetry/exporter-trace-otlp-http");
+const { BatchSpanProcessor } = require("@opentelemetry/sdk-trace-base");
 
 // This registers all instrumentation packages
 registerInstrumentations({
@@ -47,7 +33,7 @@ const resource =
 const provider = new NodeTracerProvider({
     resource: resource,
 });
-//const exporter = new ConsoleSpanExporter();
+
 const exporter = new OTLPTraceExporter({
     // optional - url default value is http://localhost:4318/v1/traces
     url: process.env.OTLP_URL || "htttp://localhost:4318/v1/traces",
