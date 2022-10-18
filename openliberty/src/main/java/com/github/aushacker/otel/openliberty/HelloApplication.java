bc4d23a6b10ac6202d22497404f8f35ef8e1d879
aushacker <25114336+aushacker@users.noreply.github.com>
2022-10-18 14:37:45 +1100

    Working Open Liberty  app

---
+++ openliberty/src/main/java/com/github/aushacker/otel/openliberty/HelloApplication.java
@@ -0,0 +1,9 @@
+package com.github.aushacker.otel.openliberty;
+
+import jakarta.ws.rs.core.Application;
+import jakarta.ws.rs.ApplicationPath;
+
+@ApplicationPath("hello")
+public class HelloApplication extends Application {
+
+}
