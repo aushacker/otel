f57b51ac8614c9e21f041318837392881ee0d6d8
aushacker <25114336+aushacker@users.noreply.github.com>
2022-10-18 14:53:33 +1100

    Simplify app uri

--- openliberty/src/main/java/com/github/aushacker/otel/openliberty/HelloApplication.java
+++ openliberty/src/main/java/com/github/aushacker/otel/openliberty/HelloApplication.java
@@ -3,7 +3,7 @@ package com.github.aushacker.otel.openliberty;
 import jakarta.ws.rs.core.Application;
 import jakarta.ws.rs.ApplicationPath;
 
-@ApplicationPath("hello")
+@ApplicationPath("")
 public class HelloApplication extends Application {
 
 }
--- openliberty/src/main/java/com/github/aushacker/otel/openliberty/HelloResource.java
+++ openliberty/src/main/java/com/github/aushacker/otel/openliberty/HelloResource.java
@@ -5,12 +5,12 @@ import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
 
-@Path("/greet")
+@Path("/hello")
 public class HelloResource {
 
     @GET
     @Produces(MediaType.APPLICATION_JSON)
     public String hello() {
-        return "Hello world";
+        return "Hello world\n";
     }
 }
